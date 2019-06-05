ffb68b888be10cbdbcda9f843e624442d9140e2a
lyus76 <50140697+lyus76@users.noreply.github.com>
2019-06-05 17:47:14 +0300

    Update add group test

--- test_add-group.py
+++ test_add-group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+
+import unittest
 
 
 def is_alert_present(wd):
@@ -12,41 +12,42 @@ def is_alert_present(wd):
         return False
 
 
-class test_add_group(unittest.TestCase):
-    def setUp(selfself):
+class TestAddGroup(unittest.TestCase):
+    def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
 
     def test_add_group(self):
         success = True
-        ws = self.wd
+        wd = self.wd
         wd.get('http://localhost/addressbook/')
         wd.find_element_by_name('user').click()
         wd.find_element_by_name('user').clear()
-        wd.find_element_by_name('user').send_key('admin')
+        wd.find_element_by_name('user').send_keys("admin")
         wd.find_element_by_name('pass').click()
         wd.find_element_by_name('pass').clear()
-        wd.find_element_by_name('pass').send_key('secret')
+        wd.find_element_by_name('pass').send_keys('secret')
         wd.find_element_by_css_selector('input[type=\"submit\"]').click()
         wd.find_element_by_link_text('groups').click()
         wd.find_element_by_name('new').click()
         wd.find_element_by_name('group_name').click()
         wd.find_element_by_name('group_name').clear()
-        wd.find_element_by_name('group_name').send_key('dfgsdf')
+        wd.find_element_by_name('group_name').send_keys('dfgsdf')
         wd.find_element_by_name('group_header').click()
         wd.find_element_by_name('group_header').clear()
-        wd.find_element_by_name('group_header').send_key('dfgsdf')
+        wd.find_element_by_name('group_header').send_keys('dfgsdf')
         wd.find_element_by_name('group_footer').click()
         wd.find_element_by_name('group_footer').clear()
-        wd.find_element_by_name('group_footer').send_key('dfgsdf')
+        wd.find_element_by_name('group_footer').send_keys('dfgsdf')
         wd.find_element_by_name('submit').click()
-        wd.find_element_by_link_text('group_page').click()
+        wd.find_element_by_link_text('group page').click()
         wd.find_element_by_link_text('Logout').click()
         self.assertTrue(success)
 
-    def teardown(selfself):
+    def teardown(self):
         self.wd.quit()
+
 
 if __name__ == '__main__':
     # execute only if run as a script
-    main()
+    unittest.main()
